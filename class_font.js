9866f6a1a44c621db280887e7a2492935fcd6490
wofsauge <jan-.-@t-online.de>
2018-12-13 22:38:30 +0100

    try to get Gitpages to work

---
+++ class_font.js
@@ -0,0 +1,4 @@
+var class_font =
+[
+    [ "Font", "class_font.html#ae12d76f0b59747bbf028333c9c7ed560", null ]
+];
---
+++ class_k_color.js
@@ -0,0 +1,18 @@
+var class_k_color =
+[
+    [ "KColor", "class_k_color.html#adad4075b6c806d97b9b0134e9e9e519d", null ],
+    [ "KColor", "class_k_color.html#a299e7ac540fe56fb4d7fcff0c7b79d35", null ],
+    [ "__mul", "class_k_color.html#ab2eab2c9d834ce658594830a33eaa45f", null ],
+    [ "Lerp", "class_k_color.html#ab5aec21730df314963db1194bf6991a2", null ],
+    [ "Reset", "class_k_color.html#ad538fbf4543001758889aa3a72bb66db", null ],
+    [ "SetColorize", "class_k_color.html#a6aa7e0ecdc2244e8aa0b7483d884d6ab", null ],
+    [ "SetOffset", "class_k_color.html#a68d484f6e298d40588e7661d17cebd5b", null ],
+    [ "SetTint", "class_k_color.html#a0d86c3ddddd03a3ee2ebd14ed1af0449", null ],
+    [ "A", "class_k_color.html#a653c47afd1e1437a9e328b57cb63dd3f", null ],
+    [ "B", "class_k_color.html#ae4868ee86a56afea2ee7b4dbae6a69ce", null ],
+    [ "BO", "class_k_color.html#afcbff3e5141636698910999910d9df14", null ],
+    [ "G", "class_k_color.html#acb303974c5f6a36b834510201dcdd398", null ],
+    [ "GO", "class_k_color.html#a2d8d7e64f446b310471889fc5945586f", null ],
+    [ "R", "class_k_color.html#af5d939dca608b9f9292239575b3128f9", null ],
+    [ "RO", "class_k_color.html#ac0b037da3921b26c7127203d6c062db8", null ]
+];
