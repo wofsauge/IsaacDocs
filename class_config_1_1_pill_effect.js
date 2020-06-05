8fd4e7f8ccac3edf6a3c4ff65fa41386b3fc6caa
wofsauge <jan-.-@t-online.de>
2020-06-05 22:39:50 +0200

    removed duplicated pages

--- class_config_1_1_pill_effect.js
+++
@@ -1,7 +0,0 @@
-var class_config_1_1_pill_effect =
-[
-    [ "AchievementID", "class_config_1_1_pill_effect.html#aa060eec3d4973f5c7eda321cd13928f0", null ],
-    [ "GreedModeAllowed", "class_config_1_1_pill_effect.html#a0aa0930bd35886addd3b3a5c0f7677b4", null ],
-    [ "ID", "class_config_1_1_pill_effect.html#a0842b881913727880ed849be66fd3dd7", null ],
-    [ "Name", "class_config_1_1_pill_effect.html#aca54627dda39d59d5d00992ce25a9d21", null ]
-];
