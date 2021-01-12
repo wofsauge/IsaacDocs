a4d4d9ffe9609e531e09a6c1be6701d0fd89b598
Wofsauge <jan-.-@t-online.de>
2021-01-12 19:59:44 +0100

    Build new version of the beta docs

--- beta/site/js/customJS.js
+++ beta/site/js/customJS.js
@@ -1,9 +1,49 @@
-$( document ).ready(function() {
-    $(".md-search").append("<button class=\"test\">Darkmode</button>");
-    $(".test").on('click', function () {
-      $("body").toggleClass("darkMode");
-    });
+$(document).ready(function() {
+    $(".md-search").append("<img onclick=\"toggleDarkMode()\" src=\"../../images/darkMode.png\" title=\"Toggle Darkmode\" class=\"darkmodeButton\" width=\"25\" height=\"25\" alt=\"darkmode\" />");
 });
+
+function toggleDarkMode() {
+    console.log("test");
+    if (typeof(Storage) !== "undefined") {
+        var darkModeState = localStorage.getItem("darkMode");
+        if (darkModeState == null || darkModeState == 0) {
+            darkModeState = 1;
+            $("body").addClass("darkMode");
+        } else {
+            darkModeState = 0;
+            $("body").removeClass("darkMode");
+        }
+        localStorage.setItem("darkMode", darkModeState);
+    } else {
+        $("body").toggleClass("darkMode");
+    }
+}
+
 app.document$.subscribe(function() {
-  $(".badge").parent().addClass("badgeLine");
+    console.log("test");
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.getItem("darkMode") == 1) {
+            $("body").addClass("darkMode");
+        }
+
+        // handle recently visited
+        var recentList = localStorage.getItem("recentlyVisited");
+        if (recentList == null) {
+            recentList = [];
+        } else {
+            recentList = recentList.split(',');
+        }
+        var pathname = window.location.pathname;
+
+        const index = recentList.indexOf(pathname);
+        if (index > -1) {
+            recentList.splice(index, 1);
+        }
+        recentList.unshift(pathname);
+        recentList.splice(10, 1);
+        localStorage.setItem("recentlyVisited", recentList.toString());
+    }
+
+    // handle badge line object
+    $(".badge").parent().addClass("badgeLine");
 });
