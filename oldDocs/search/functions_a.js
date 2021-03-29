82f17f76fe51cf1e25bfede844167c76f0eb4dda
GitHub Action <github-action@users.noreply.github.com>
2021-03-29 23:18:14 +0000

    Removed everything with mike 0.5.5

--- oldDocs/search/functions_a.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['color',['Color',['../class_color.html#a299e7ac540fe56fb4d7fcff0c7b79d35',1,'Color::Color(float, float, float, float, integer, integer, integer)']]],
-  ['kcolor',['KColor',['../class_color.html#adad4075b6c806d97b9b0134e9e9e519d',1,'Color::Color(float, float, float, float)']]],
-  ['keepdoorsclosed',['KeepDoorsClosed',['../class_room.html#af3c973def5cd03cbf4c26cf2ac475846',1,'Room']]],
-  ['kill',['Kill',['../class_entity.html#a9b0079ee3d02f1a8d6e4acfd88a19fd3',1,'Entity']]],
-  ['killunique',['KillUnique',['../class_entity_n_p_c.html#a7385aea1f4eeb7377caed1fc41c82df5',1,'EntityNPC']]]
-];
