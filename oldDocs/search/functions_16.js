bdbf1967420eb458068284ef0396b3d4f0a89b7e
GitHub Action <github-action@users.noreply.github.com>
2021-03-29 22:49:57 +0000

    Removed everything with mike 0.5.5

--- oldDocs/search/functions_16.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['waseventtriggered',['WasEventTriggered',['../class_sprite.html#a1ed50d281e74b764fa99108f82158b90',1,'Sprite']]],
-  ['willplayerrevive',['WillPlayerRevive',['../class_entity_player.html#a76ada5c2648c15f6b51a68a36373c01c',1,'EntityPlayer']]],
-  ['worldtorenderposition',['WorldToRenderPosition',['../namespace_isaac.html#affff7aff8ffde4504e8f9ac3478e5e25',1,'Isaac']]],
-  ['worldtoscreen',['WorldToScreen',['../namespace_isaac.html#acdf41db0c164ffb653695a5a13b6033c',1,'Isaac']]],
-  ['worldtoscreendistance',['WorldToScreenDistance',['../namespace_isaac.html#a3e5b7bafe2f016f01a3478e07b18bc49',1,'Isaac']]],
-  ['worldtoscreenposition',['WorldToScreenPosition',['../class_room.html#a2f938a89690419bd82964b2a60d86400',1,'Room']]]
-];
