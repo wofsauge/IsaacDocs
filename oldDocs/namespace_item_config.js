adbcfeb39c47087b0100cb9e96c74ff37e43d973
wofsauge <wofsauge@users.noreply.github.com>
2021-06-04 12:36:13 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/namespace_item_config.js
@@ -0,0 +1,8 @@
+var namespace_item_config =
+[
+    [ "Card", "class_item_config_1_1_card.html", "class_item_config_1_1_card" ],
+    [ "Config", "class_item_config_1_1_config.html", "class_item_config_1_1_config" ],
+    [ "Costume", "class_item_config_1_1_costume.html", "class_item_config_1_1_costume" ],
+    [ "Item", "class_item_config_1_1_item.html", "class_item_config_1_1_item" ],
+    [ "PillEffect", "class_item_config_1_1_pill_effect.html", "class_item_config_1_1_pill_effect" ]
+];
