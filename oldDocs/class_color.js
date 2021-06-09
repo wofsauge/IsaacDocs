51269ef8729765a20d1d5b54e29710eb59608777
im-tem <im-tem@users.noreply.github.com>
2021-06-09 06:00:59 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_color.js
@@ -0,0 +1,17 @@
+var class_color =
+[
+    [ "Color", "class_color.html#abfab99e1ad10cdb66e5f8cfd7d273cd6", null ],
+    [ "__mul", "class_color.html#a2041b94436a53fc1403f6f7026584724", null ],
+    [ "Lerp", "class_color.html#a8d482383815756ab0974b34a6b8add75", null ],
+    [ "Reset", "class_color.html#a36fa4bb53ae01f54533811a89852040b", null ],
+    [ "SetColorize", "class_color.html#a86fc92e43adbf87ff84f52c583f1e476", null ],
+    [ "SetOffset", "class_color.html#a5f88bdd6845755c7d6a9a4cfee899467", null ],
+    [ "SetTint", "class_color.html#ad49d0d20bea3de0cf31ea9d0babf68a0", null ],
+    [ "A", "class_color.html#a44c3ff4cf70465b77c937f2f72d4bac8", null ],
+    [ "B", "class_color.html#a2b625cd3df94d1192c6f6aac31fe1f98", null ],
+    [ "BO", "class_color.html#a20cc85ed5c20dca7ed3b1bbb964b3b43", null ],
+    [ "G", "class_color.html#a481d25488e6b2669f4d1819f5f15f8b3", null ],
+    [ "GO", "class_color.html#a84b562ec1961d8efbfe1d17cf6cc5b66", null ],
+    [ "R", "class_color.html#afb8ce8200a490088d415606620149b82", null ],
+    [ "RO", "class_color.html#a1d15c3b27349d4374e18a0e5eaf400ad", null ]
+];
---
+++ oldDocs/search/functions_e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['open',['Open',['../class_grid_entity_door.html#a21df3aebbe10d44b92d020c0b778dddc',1,'GridEntityDoor']]]
+];
