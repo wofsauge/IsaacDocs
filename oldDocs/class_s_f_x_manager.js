2871d32b0bfecd88e710bf33ca16debb1b6cdde8
wofsauge <wofsauge@users.noreply.github.com>
2021-06-18 19:40:23 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_s_f_x_manager.js
@@ -0,0 +1,12 @@
+var class_s_f_x_manager =
+[
+    [ "AdjustPitch", "class_s_f_x_manager.html#aecaefa3f3e8e92a13e52426145ee909a", null ],
+    [ "AdjustVolume", "class_s_f_x_manager.html#a25aa6fb3f0adcd1f97429a44bcdbae98", null ],
+    [ "GetAmbientSoundVolume", "class_s_f_x_manager.html#a4ea4a0018b172bfefc978b2987de08cf", null ],
+    [ "IsPlaying", "class_s_f_x_manager.html#aab2b0a086d5c46130fd12cea6838f5c3", null ],
+    [ "Play", "class_s_f_x_manager.html#ad5a2b5686d31b654db806a9755334d9f", null ],
+    [ "Preload", "class_s_f_x_manager.html#a97da5566667bbd63a6bc6375b8e43bf9", null ],
+    [ "SetAmbientSound", "class_s_f_x_manager.html#a0f713773136bed8183750c96970ae3da", null ],
+    [ "Stop", "class_s_f_x_manager.html#ab1901aa2c66c9c28294768ab965d8b45", null ],
+    [ "StopLoopingSounds", "class_s_f_x_manager.html#a7e89c72f45258a29bd2bc3ab77168b0d", null ]
+];
