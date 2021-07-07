4fc1b3563d38ab96c02e9fe1b1a96c12a28afdac
wofsauge <wofsauge@users.noreply.github.com>
2021-07-07 17:56:57 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/search/enums_3.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['damageflag',['DamageFlag',['../group__enums.html#gab084d3a6d6ddd144ea6df4af965f93fc',1,'Enums.h']]],
+  ['difficulty',['Difficulty',['../group__enums.html#ga43efb0a2e61815a66134dfa5ccea0a83',1,'Enums.h']]],
+  ['direction',['Direction',['../group__enums.html#ga224b9163917ac32fc95a60d8c1eec3aa',1,'Enums.h']]],
+  ['doorslot',['DoorSlot',['../group__enums.html#ga445b41ae2021fde99dff29df01f9ce38',1,'Enums.h']]],
+  ['doorstate',['DoorState',['../group__enums.html#ga117f8df7c03b73093e6da94ae41b4252',1,'Enums.h']]],
+  ['doorvariant',['DoorVariant',['../group__enums.html#ga0f3da80bafaf320a181143843937f59c',1,'Enums.h']]]
+];
---
+++ oldDocs/search/variables_17.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['y',['Y',['../class_room_config_1_1_spawn.html#a674979e6a64139e403d07b62a171c050',1,'RoomConfig::Spawn']]]
+];
