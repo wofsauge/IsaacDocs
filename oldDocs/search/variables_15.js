a1b6a8b5524e4ef82004092dd860d56f73e6a05f
GitHub Action <github-action@users.noreply.github.com>
2021-07-27 22:03:01 +0000

    Removed everything with mike 1.0.1

--- oldDocs/search/variables_15.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['wait',['Wait',['../class_entity_pickup.html#a8b71d81bcdb9091edae700e4f7ceba2c',1,'EntityPickup']]],
-  ['waitframes',['WaitFrames',['../class_entity_tear.html#a3a74cf42486e5ed599cfed666c96bdad',1,'EntityTear']]],
-  ['weight',['Weight',['../class_room_config_1_1_entry.html#a61b848a7ed5acb126e1e79f54ffc8caa',1,'RoomConfig::Entry']]],
-  ['wiggleframeoffset',['WiggleFrameOffset',['../class_projectile_params.html#adc5d3b878ff4cd8430b22614413e630b',1,'ProjectileParams::WiggleFrameOffset()'],['../class_entity_projectile.html#a7d379e0cb6425e2e8dd2e754976ae875',1,'EntityProjectile::WiggleFrameOffset()']]]
-];
