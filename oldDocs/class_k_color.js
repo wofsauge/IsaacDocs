fe1b3b7cb4fc7641612c0480254cd8e43a125fd7
GitHub Action <github-action@users.noreply.github.com>
2021-07-27 20:59:52 +0000

    Removed everything with mike 1.0.1

--- oldDocs/class_k_color.js
+++
@@ -1,8 +0,0 @@
-var class_k_color =
-[
-    [ "KColor", "class_k_color.html#adad4075b6c806d97b9b0134e9e9e519d", null ],
-    [ "A", "class_k_color.html#a653c47afd1e1437a9e328b57cb63dd3f", null ],
-    [ "B", "class_k_color.html#ae4868ee86a56afea2ee7b4dbae6a69ce", null ],
-    [ "G", "class_k_color.html#acb303974c5f6a36b834510201dcdd398", null ],
-    [ "R", "class_k_color.html#af5d939dca608b9f9292239575b3128f9", null ]
-];
