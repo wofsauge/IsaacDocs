b6d5549cd8006057eb20bac0210593b147dce3d8
wofsauge <wofsauge@users.noreply.github.com>
2021-08-17 20:50:21 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/search/variables_4.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['endpoint',['EndPoint',['../class_entity_laser.html#a675f84ba91a4db4be0fdcfc4ae5151b0',1,'EntityLaser']]],
+  ['enterdoor',['EnterDoor',['../class_level.html#aafde153b89894735004cd080cf5cc2f3',1,'Level']]],
+  ['entity',['Entity',['../class_entity_ref.html#a39baed474c959ac105d47a0370f3b031',1,'EntityRef']]],
+  ['entitycollisionclass',['EntityCollisionClass',['../class_entity.html#aeb3a16749d2f8bcca56f921556f443e8',1,'Entity']]],
+  ['entityref',['EntityRef',['../class_entity_n_p_c.html#abe6f5c6979aed26ada4576dbf2aaac25',1,'EntityNPC']]],
+  ['entrycount',['EntryCount',['../class_room_config_1_1_spawn.html#a2b33332973cbc72b460ab6232ba336f4',1,'RoomConfig::Spawn']]],
+  ['explosiondamage',['ExplosionDamage',['../class_entity_bomb.html#ad0e5b2887ebae55ed701b0be640fe22a',1,'EntityBomb']]],
+  ['extrasprite',['ExtraSprite',['../class_grid_entity_door.html#af0fb498054d93363aa54fcca4c2f8abe',1,'GridEntityDoor']]],
+  ['extravisible',['ExtraVisible',['../class_grid_entity_door.html#a81403141eef1268091d2c875ea1a6378',1,'GridEntityDoor']]]
+];
