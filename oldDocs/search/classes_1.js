2432d7b9917792af6acdbe0f5a11bf543199cbda
GitHub Action <github-action@users.noreply.github.com>
2021-08-25 18:03:50 +0000

    Removed everything with mike 1.0.1

--- oldDocs/search/classes_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['card',['Card',['../class_item_config_1_1_card.html',1,'ItemConfig']]],
-  ['cardconfiglist',['CardConfigList',['../class_cpp_container_1_1_vector_1_1_card_config_list.html',1,'CppContainer::Vector']]],
-  ['config',['Config',['../class_item_config_1_1_config.html',1,'ItemConfig']]],
-  ['costume',['Costume',['../class_item_config_1_1_costume.html',1,'ItemConfig']]],
-  ['costumeconfiglist',['CostumeConfigList',['../class_cpp_container_1_1_vector_1_1_costume_config_list.html',1,'CppContainer::Vector']]]
-];
