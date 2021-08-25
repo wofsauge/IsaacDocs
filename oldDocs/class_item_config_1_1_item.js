45389d2b4f6000b3be52871a2bbd61ab84e4cbda
wofsauge <wofsauge@users.noreply.github.com>
2021-08-25 18:04:31 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_item_config_1_1_item.js
@@ -0,0 +1,26 @@
+var class_item_config_1_1_item =
+[
+    [ "IsCollectible", "class_item_config_1_1_item.html#a3773b437fb0f39bb7f50bbe9f372f944", null ],
+    [ "IsNull", "class_item_config_1_1_item.html#a276dc8ad803430119c914b7ef889a6fb", null ],
+    [ "IsTrinket", "class_item_config_1_1_item.html#aafa627205da7ad22004c48fa41f5f026", null ],
+    [ "AchievementID", "class_item_config_1_1_item.html#a7b9005919c89520156e4e539a500836a", null ],
+    [ "AddBlackHearts", "class_item_config_1_1_item.html#ab546750be95f7a5c74c53312634b3a7c", null ],
+    [ "AddBombs", "class_item_config_1_1_item.html#ac68e4f03aa857268a3326ea706d536c5", null ],
+    [ "AddCoins", "class_item_config_1_1_item.html#aba9d66daa9366ca721679c6f445db026", null ],
+    [ "AddHearts", "class_item_config_1_1_item.html#a03b512ef36a75ecb1a536f58606bc945", null ],
+    [ "AddKeys", "class_item_config_1_1_item.html#a2a2e72a698ea06661b152a1c7d1df970", null ],
+    [ "AddMaxHearts", "class_item_config_1_1_item.html#a012c99aaac461381656d83963c403c0c", null ],
+    [ "AddSoulHearts", "class_item_config_1_1_item.html#a007c9e68363564d4608cafc1d27c0514", null ],
+    [ "CacheFlags", "class_item_config_1_1_item.html#a542eeff5a25ffeac86a8a8a312b76f2c", null ],
+    [ "Costume", "class_item_config_1_1_item.html#a60dfb748978338720d9dfae9c8c3d476", null ],
+    [ "Description", "class_item_config_1_1_item.html#a458902cc7e352e3755123093ebfb8a42", null ],
+    [ "DevilPrice", "class_item_config_1_1_item.html#a5f209804f4e0fcdc09f05359c63843a9", null ],
+    [ "Discharged", "class_item_config_1_1_item.html#a2c1239c9a5a599e96f1066432afd184f", null ],
+    [ "GfxFileName", "class_item_config_1_1_item.html#a47c5e462fca36b280577176197c02873", null ],
+    [ "ID", "class_item_config_1_1_item.html#a01a8a96e778f5d4ae86705dab32774bd", null ],
+    [ "MaxCharges", "class_item_config_1_1_item.html#aad64574ccb04066f5203336b17d5687d", null ],
+    [ "MaxCooldown", "class_item_config_1_1_item.html#ab053522249a7ff7816c832ea849dbf93", null ],
+    [ "Name", "class_item_config_1_1_item.html#aee05249c734db6597d8f20202a4b68f5", null ],
+    [ "Special", "class_item_config_1_1_item.html#a31a7e5dad4e17002b371da96642b9e7c", null ],
+    [ "Type", "class_item_config_1_1_item.html#a4eb09e16fd90b3afafbe6f54f8bae06d", null ]
+];
