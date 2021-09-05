58734b900b68088c264d9f3efd9c7ed78b5231fa
Zamiell <Zamiell@users.noreply.github.com>
2021-09-05 15:37:51 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_item_config_1_1_costume.js
@@ -0,0 +1,11 @@
+var class_item_config_1_1_costume =
+[
+    [ "Anm2Path", "class_item_config_1_1_costume.html#ae0f6bbcf219251912fd715ef8833340a", null ],
+    [ "HasOverlay", "class_item_config_1_1_costume.html#a9d1d32b4adb6f0b9af78e410e5ab9cdc", null ],
+    [ "HasSkinAlt", "class_item_config_1_1_costume.html#aee4628d3067108eaa842888c98862ffa", null ],
+    [ "ID", "class_item_config_1_1_costume.html#ab9d876d2008a2378778750f2cb773c13", null ],
+    [ "IsFlying", "class_item_config_1_1_costume.html#a38ef336e1e1c4f62cce389f1f76ac9e8", null ],
+    [ "OverwriteColor", "class_item_config_1_1_costume.html#a323a064a49ab51738a46a215dd2db446", null ],
+    [ "Priority", "class_item_config_1_1_costume.html#ab7ff9e7b1f7ea7c6650bccf09d67867e", null ],
+    [ "SkinColor", "class_item_config_1_1_costume.html#a1707367bc1a5333dff4a7367cbebaa0b", null ]
+];
