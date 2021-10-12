b93ea873809ebc730705b0d82d56a8f1c53da606
Zamiell <Zamiell@users.noreply.github.com>
2021-10-12 01:14:29 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_item_config_1_1_config.js
@@ -0,0 +1,16 @@
+var class_item_config_1_1_config =
+[
+    [ "GetCard", "class_item_config_1_1_config.html#af01a4b34e0574b956c291bff48bcc0f6", null ],
+    [ "GetCards", "class_item_config_1_1_config.html#af2d8c59d30d76bf14700db1736a71e88", null ],
+    [ "GetCollectible", "class_item_config_1_1_config.html#a113997460c5070a7da2715d1e1003889", null ],
+    [ "GetCollectibles", "class_item_config_1_1_config.html#ab570e24606197f78a9cf4476dba71ef5", null ],
+    [ "GetCostumes", "class_item_config_1_1_config.html#a4ad35a29215494b7c814841c8d09b280", null ],
+    [ "GetNullItem", "class_item_config_1_1_config.html#a39164fcef99fd373821e7564a0a013c1", null ],
+    [ "GetNullItems", "class_item_config_1_1_config.html#a367f5288d7770455f1cba77582a46bf2", null ],
+    [ "GetPillEffect", "class_item_config_1_1_config.html#acbbc9c39e44e4bae548c82f7a820bd5e", null ],
+    [ "GetPillEffects", "class_item_config_1_1_config.html#a448477bee9c7d7d74c4828c57068e533", null ],
+    [ "GetTrinket", "class_item_config_1_1_config.html#ac3bce895596cc6c6c2379fac4cf003ed", null ],
+    [ "GetTrinkets", "class_item_config_1_1_config.html#a97d611f689f2e54b04ee685cd430b3e0", null ],
+    [ "IsValidCollectible", "class_item_config_1_1_config.html#ac54e567916e0aa66e61786b1f065979b", null ],
+    [ "ShouldAddCostumeOnPickup", "class_item_config_1_1_config.html#a081bc308ad482c33ba4b69bf7c54fdb4", null ]
+];
