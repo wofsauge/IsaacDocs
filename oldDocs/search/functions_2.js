51fcce5503bbd43d4c4b920417bfdcf91149c9c0
GitHub Action <github-action@users.noreply.github.com>
2021-10-20 15:50:16 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/functions_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['bar',['Bar',['../class_grid_entity_door.html#aaeae536b8e1cc0b5afb7a66987e39b94',1,'GridEntityDoor']]],
-  ['bloodexplode',['BloodExplode',['../class_entity.html#a3e9415049664ac823377040bb8067787',1,'Entity']]],
-  ['bombdamage',['BombDamage',['../class_game.html#a68ba4f8390d99eb2ebc64c7484fb341d',1,'Game']]],
-  ['bombexplosioneffects',['BombExplosionEffects',['../class_game.html#aafb01ef0e05dafa3750edbe214df6718',1,'Game']]],
-  ['bombtearflageffects',['BombTearflagEffects',['../class_game.html#ad809b288d39d863d0e54dcce8e48ea6d',1,'Game']]],
-  ['butterbeanfart',['ButterBeanFart',['../class_game.html#aa0adcb328556d4e421726d3ac046ebac',1,'Game']]]
-];
--- oldDocs/search/variables_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['name',['Name',['../class_item_config_1_1_card.html#ad49c386393b1b5827284b50abc98c891',1,'ItemConfig::Card::Name()'],['../class_item_config_1_1_pill_effect.html#a7a174313ade7878c0989aa7d9967aebf',1,'ItemConfig::PillEffect::Name()'],['../class_item_config_1_1_item.html#aee05249c734db6597d8f20202a4b68f5',1,'ItemConfig::Item::Name()']]],
-  ['nextgreedanimation',['NextGreedAnimation',['../class_grid_entity_pressure_plate.html#a21fcc45ad3d7ebd55f07bcdab2a4eb83',1,'GridEntityPressurePlate']]],
-  ['noreward',['NoReward',['../class_room_descriptor.html#a59899e8c67588113087ca2605ca70b34',1,'RoomDescriptor']]]
-];
