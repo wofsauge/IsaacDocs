6363b96988a75e45096d5332a6025d48e673bbd8
GitHub Action <github-action@users.noreply.github.com>
2021-11-11 19:22:47 +0000

    Removed everything with mike 1.1.2

--- oldDocs/modules.js
+++
@@ -1,5 +0,0 @@
-var modules =
-[
-    [ "Enumerations", "group__enums.html", "group__enums" ],
-    [ "Functions", "group__funcs.html", "group__funcs" ]
-];
--- oldDocs/search/namespaces_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['input',['Input',['../namespace_input.html',1,'']]],
-  ['isaac',['Isaac',['../namespace_isaac.html',1,'']]],
-  ['itemconfig',['ItemConfig',['../namespace_item_config.html',1,'']]]
-];
