a6b220b4fa885d42e61e43107ad0e45477b97613
GitHub Action <github-action@users.noreply.github.com>
2021-11-11 17:52:05 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/classes_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['rng',['RNG',['../class_r_n_g.html',1,'']]],
-  ['room',['Room',['../class_room_config_1_1_room.html',1,'RoomConfig::Room'],['../class_room.html',1,'Room']]],
-  ['roomdescriptor',['RoomDescriptor',['../class_room_descriptor.html',1,'']]]
-];
