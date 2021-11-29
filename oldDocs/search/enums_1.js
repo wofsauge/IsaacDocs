981ba49a54bb456abc4fb89ad63bc86316b698b1
Zamiell <Zamiell@users.noreply.github.com>
2021-11-29 20:33:27 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/search/enums_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['babysubtype',['BabySubType',['../group__enums.html#gad65102fdce2fc7b437b3d73d7753e747',1,'Enums.h']]],
+  ['bombsubtype',['BombSubType',['../group__enums.html#gaa1b9e505508723c6c42f761b3eec48b6',1,'Enums.h']]],
+  ['bombvariant',['BombVariant',['../group__enums.html#gadf74ad42bd2a6d6963e6d84b54385174',1,'Enums.h']]],
+  ['buttonaction',['ButtonAction',['../group__enums.html#gafa717ac273a5a382f7c01ef7afba1ee7',1,'Enums.h']]]
+];
---
+++ oldDocs/search/variables_d.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['offset',['Offset',['../class_sprite.html#aa674bf8daad990e61dcb43a524b6bb96',1,'Sprite']]],
+  ['onehit',['OneHit',['../class_entity_laser.html#af6e3f7b8cc5cfe3b2e04a8b78d799b30',1,'EntityLaser']]],
+  ['openanimation',['OpenAnimation',['../class_grid_entity_door.html#a383a2edbefe6ed3f42361420a2adfec0',1,'GridEntityDoor']]],
+  ['openlockedanimation',['OpenLockedAnimation',['../class_grid_entity_door.html#a97ae6b13424c54ad1cca95e06cdd4f27',1,'GridEntityDoor']]],
+  ['orbitangleoffset',['OrbitAngleOffset',['../class_entity_familiar.html#a2919cd1350a1a14da8d9f1528cd7ad42',1,'EntityFamiliar']]],
+  ['orbitdistance',['OrbitDistance',['../class_entity_familiar.html#ae09a3876ae274ce071acd2d12feae2f5',1,'EntityFamiliar']]],
+  ['overridedata',['OverrideData',['../class_room_descriptor.html#a922e95692f4b927ede4fc18553fb43f4',1,'RoomDescriptor']]],
+  ['overwritecolor',['OverwriteColor',['../class_item_config_1_1_costume.html#a323a064a49ab51738a46a215dd2db446',1,'ItemConfig::Costume']]]
+];
