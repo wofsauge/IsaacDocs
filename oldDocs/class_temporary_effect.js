5ada7617f0aec291518609f6bc6487ec91545d88
GitHub Action <github-action@users.noreply.github.com>
2021-12-04 01:20:25 +0000

    Removed everything with mike 1.1.2

--- oldDocs/class_temporary_effect.js
+++
@@ -1,6 +0,0 @@
-var class_temporary_effect =
-[
-    [ "Cooldown", "class_temporary_effect.html#aba56ce61589b1e646d84b84b172ec92d", null ],
-    [ "Count", "class_temporary_effect.html#a13fdcfda0ee6851fbaaa32d2d579acd6", null ],
-    [ "Item", "class_temporary_effect.html#ad71ae0e9928ef2cf5ba67e09bb50fa7e", null ]
-];
