06e25583646282766fbcd8df83ee25aa635b44ef
Zamiell <Zamiell@users.noreply.github.com>
2021-12-04 14:12:27 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_item_config_1_1_pill_effect.js
@@ -0,0 +1,7 @@
+var class_item_config_1_1_pill_effect =
+[
+    [ "AchievementID", "class_item_config_1_1_pill_effect.html#ae44cc4d382630f5f2dad4aa935a45d98", null ],
+    [ "GreedModeAllowed", "class_item_config_1_1_pill_effect.html#a04ff3d4dc4bd9afba99e5f16b8f43827", null ],
+    [ "ID", "class_item_config_1_1_pill_effect.html#a2a74a4a386325a2ed5402e0977791cd7", null ],
+    [ "Name", "class_item_config_1_1_pill_effect.html#a7a174313ade7878c0989aa7d9967aebf", null ]
+];
---
+++ oldDocs/search/enumvalues_15.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['xray_5fwall',['XRAY_WALL',['../group__enums.html#gga76f2089184023be252ef251921626d99a82b61c1a1fb71d974a6e76bce766b715',1,'Enums.h']]]
+];
