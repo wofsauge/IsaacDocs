aac02ad301bbea34f1e4954adc61b193cbaea78f
wofsauge <wofsauge@users.noreply.github.com>
2022-02-20 18:16:35 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_temporary_effects.js
@@ -0,0 +1,20 @@
+var class_temporary_effects =
+[
+    [ "AddCollectibleEffect", "class_temporary_effects.html#a225fd65e951189f3cc96ebbda4a19e40", null ],
+    [ "AddNullEffect", "class_temporary_effects.html#aa13bb054e0d6a84af3ba5770617a2348", null ],
+    [ "AddTrinketEffect", "class_temporary_effects.html#ad352c02d13a693225933cc332000bc20", null ],
+    [ "ClearEffects", "class_temporary_effects.html#ac8a8f502c86d7c92d9ab16202d616ca8", null ],
+    [ "GetCollectibleEffect", "class_temporary_effects.html#a33d1b8cdd8b169cc346e33a0553647f8", null ],
+    [ "GetCollectibleEffectNum", "class_temporary_effects.html#abf6dc6849ae5f75fe0156d9e1c9b83fe", null ],
+    [ "GetEffectsList", "class_temporary_effects.html#a2f168bf5fdd8d876829f24c340944db5", null ],
+    [ "GetNullEffect", "class_temporary_effects.html#aaf49f94b947e51388e93b78d1eed8d08", null ],
+    [ "GetNullEffectNum", "class_temporary_effects.html#a3ee27bef58cd4476c81a3b8d812a43db", null ],
+    [ "GetTrinketEffect", "class_temporary_effects.html#a7dd97a7dc421580d26c5d7ef40a77bb4", null ],
+    [ "GetTrinketEffectNum", "class_temporary_effects.html#a332cc5b6381c0adb22223f1f58855882", null ],
+    [ "HasCollectibleEffect", "class_temporary_effects.html#a4ff0e511a4cb043e8be18b8de7ebeef1", null ],
+    [ "HasNullEffect", "class_temporary_effects.html#ae4a6aaa0d0050d1aab9f71fe85c94914", null ],
+    [ "HasTrinketEffect", "class_temporary_effects.html#aa860b712b869bb473ced6576c885da4f", null ],
+    [ "RemoveCollectibleEffect", "class_temporary_effects.html#a81bf9027bedf498dcaf80577864c388d", null ],
+    [ "RemoveNullEffect", "class_temporary_effects.html#afc37456e353259624fc6418866f6a510", null ],
+    [ "RemoveTrinketEffect", "class_temporary_effects.html#a3a58da4529f7471e8ec54d1b0660842a", null ]
+];
