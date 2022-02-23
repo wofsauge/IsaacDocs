8a0c3263306faa4435625372b55fc951c31997d9
Zamiell <Zamiell@users.noreply.github.com>
2022-02-23 22:29:17 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/namespaces.js
@@ -0,0 +1,9 @@
+var namespaces =
+[
+    [ "CppContainer", "namespace_cpp_container.html", "namespace_cpp_container" ],
+    [ "Input", "namespace_input.html", null ],
+    [ "Isaac", "namespace_isaac.html", null ],
+    [ "ItemConfig", "namespace_item_config.html", null ],
+    [ "PlayerTypes", "namespace_player_types.html", null ],
+    [ "RoomConfig", "namespace_room_config.html", null ]
+];
