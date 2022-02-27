4e5abed85875d1aaef9415df475efc64336c287a
GitHub Action <github-action@users.noreply.github.com>
2022-02-27 15:17:46 +0000

    Removed everything with mike 1.1.2

--- oldDocs/namespace_cpp_container_1_1_array_proxy.js
+++
@@ -1,4 +0,0 @@
-var namespace_cpp_container_1_1_array_proxy =
-[
-    [ "RoomList", "class_cpp_container_1_1_array_proxy_1_1_room_list.html", "class_cpp_container_1_1_array_proxy_1_1_room_list" ]
-];
--- oldDocs/search/variables_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['color',['Color',['../class_sprite.html#a972d6a3dff4ee96fcaf48911c109170e',1,'Sprite::Color()'],['../class_projectile_params.html#a97600388504ecef4b9506abc5a7e0faa',1,'ProjectileParams::Color()']]],
-  ['keys',['Keys',['../class_entity_familiar.html#a0bcb7cb85e60b04bc86e993473ff12cd',1,'EntityFamiliar']]],
-  ['knockbackmultiplier',['KnockbackMultiplier',['../class_entity_tear.html#aff4468f597e27f94779ffb0f9d6585ab',1,'EntityTear']]]
-];
