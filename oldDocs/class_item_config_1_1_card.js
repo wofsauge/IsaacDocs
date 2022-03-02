71a4f948e11fed8a8d37b985c264565b4a4abeeb
wofsauge <wofsauge@users.noreply.github.com>
2022-03-02 00:51:20 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_item_config_1_1_card.js
@@ -0,0 +1,9 @@
+var class_item_config_1_1_card =
+[
+    [ "AchievementID", "class_item_config_1_1_card.html#a3cd1284eb6665ecd94021a9bebea58cc", null ],
+    [ "Description", "class_item_config_1_1_card.html#a51a78693378d5b97423a04b7a114ea86", null ],
+    [ "GreedModeAllowed", "class_item_config_1_1_card.html#a5c1c5f7dc2c99ba1838dce3e8d220394", null ],
+    [ "HudAnim", "class_item_config_1_1_card.html#ad531d3df25bd272c6620ee923ea40d53", null ],
+    [ "ID", "class_item_config_1_1_card.html#a7170fb2d026670fbbdf13bc24115ff86", null ],
+    [ "Name", "class_item_config_1_1_card.html#ad49c386393b1b5827284b50abc98c891", null ]
+];
