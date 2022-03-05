0ca6586dcd22483bb7b496503dddda540b44108d
Zamiell <Zamiell@users.noreply.github.com>
2022-03-05 22:21:33 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/search/variables_b.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['m_5fheight',['m_Height',['../class_entity_effect.html#a2cd09c460bf293076e3daff2ae6e1fbd',1,'EntityEffect']]],
+  ['mass',['Mass',['../class_entity.html#ae83699a74bd5f2fd33207ea45912f787',1,'Entity']]],
+  ['maxcharges',['MaxCharges',['../class_item_config_1_1_item.html#aad64574ccb04066f5203336b17d5687d',1,'ItemConfig::Item']]],
+  ['maxcooldown',['MaxCooldown',['../class_item_config_1_1_item.html#ab053522249a7ff7816c832ea849dbf93',1,'ItemConfig::Item']]],
+  ['maxdistance',['MaxDistance',['../class_entity_laser.html#a68d9a4c1841ed08a63e424d2f0e4c644',1,'EntityLaser::MaxDistance()'],['../class_entity_knife.html#ad3eb1c4ce8d1c798447f05dacc4c591a',1,'EntityKnife::MaxDistance()']]],
+  ['maxfiredelay',['MaxFireDelay',['../class_entity_player.html#aab893842a6dd2de9fb7cb5ecf0e26952',1,'EntityPlayer']]],
+  ['maxhitpoints',['MaxHitPoints',['../class_entity.html#a681002b7bcfcad2498ae864280b860e4',1,'Entity']]],
+  ['maxradius',['MaxRadius',['../class_entity_effect.html#a0b0f1dd0bb29f6830e3217b4c358f844',1,'EntityEffect']]],
+  ['minradius',['MinRadius',['../class_entity_effect.html#a3a9f5a0bf2573a2d26611f066e14411c',1,'EntityEffect']]],
+  ['movedirection',['MoveDirection',['../class_entity_familiar.html#ad75442cbc49ea87d1f1a522a3efe2864',1,'EntityFamiliar']]],
+  ['movespeed',['MoveSpeed',['../class_entity_player.html#a754dc33235da0b63f2e67046eb0e8316',1,'EntityPlayer']]]
+];
