8073a7b13d756ea17e096fa4daf68a8bb2ec674e
wofsauge <wofsauge@users.noreply.github.com>
2022-03-09 00:33:00 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_entity_knife.js
@@ -0,0 +1,19 @@
+var class_entity_knife =
+[
+    [ "GetKnifeDistance", "class_entity_knife.html#afd62f1e2f7d185506683e17e367a717b", null ],
+    [ "GetKnifeVelocity", "class_entity_knife.html#ad0741181da5b1691197e8505af7f4f27", null ],
+    [ "GetRenderZ", "class_entity_knife.html#aeca1767d8d8a9125e3440fd13ee56e13", null ],
+    [ "IsFlying", "class_entity_knife.html#a75aeb9046c361def77c9796aefd4cceb", null ],
+    [ "Reset", "class_entity_knife.html#af363c9aa93bb147e1f74fb9d88892132", null ],
+    [ "SetPathFollowSpeed", "class_entity_knife.html#ae1e1d4bec57e263837a6e9c397dff0e5", null ],
+    [ "Shoot", "class_entity_knife.html#a92bc954600ed55cb92fe2125ac247432", null ],
+    [ "Charge", "class_entity_knife.html#a5f51d65bf454fee284f5f909073deb05", null ],
+    [ "IsFlying", "class_entity_knife.html#a042fb48c8f2586b1bd1e6e4953c89830", null ],
+    [ "MaxDistance", "class_entity_knife.html#ad3eb1c4ce8d1c798447f05dacc4c591a", null ],
+    [ "PathFollowSpeed", "class_entity_knife.html#a469fc0930e9f3b407a73dcf9bccfb42e", null ],
+    [ "PathOffset", "class_entity_knife.html#a1ac7c7e803da2debf12f92a27631bf6a", null ],
+    [ "Rotation", "class_entity_knife.html#a2ae9d224f1990557819b794d66cf8007", null ],
+    [ "RotationOffset", "class_entity_knife.html#ab9242f621027724a64f18f0bbc1355b7", null ],
+    [ "Scale", "class_entity_knife.html#a0067319b755527c29660e505d01bbe84", null ],
+    [ "TearFlags", "class_entity_knife.html#a0024975171458374acbbf6b3ecc09440", null ]
+];
