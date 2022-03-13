fca8a418d30e096a14c1d7cda5f0ec633efa154e
GitHub Action <github-action@users.noreply.github.com>
2022-03-13 11:26:23 +0000

    Removed everything with mike 1.1.2

--- oldDocs/class_player_types_1_1_active_item_desc.js
+++
@@ -1,7 +0,0 @@
-var class_player_types_1_1_active_item_desc =
-[
-    [ "BatteryCharge", "class_player_types_1_1_active_item_desc.html#ada37bf4d631920c171f6bd3f67480ff9", null ],
-    [ "Charge", "class_player_types_1_1_active_item_desc.html#ae65f9aad9ed3d544592f8878a6775a25", null ],
-    [ "Item", "class_player_types_1_1_active_item_desc.html#a213c90ec5db87e826ab527244f585adc", null ],
-    [ "Lock", "class_player_types_1_1_active_item_desc.html#ad4be4f24107a9f630bf4575b4211ab5e", null ]
-];
--- oldDocs/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['using_20zerobrane_20studio_20to_20debug_20your_20scripts',['Using ZeroBrane Studio to debug your scripts',['../debugging.html',1,'']]]
-];
