19d8c8d130571b92bf91c58e17f6061c061e3515
GitHub Action <github-action@users.noreply.github.com>
2022-03-14 13:44:08 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/classes_d.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['tearparams',['TearParams',['../class_tear_params.html',1,'']]],
-  ['temporaryeffect',['TemporaryEffect',['../class_temporary_effect.html',1,'']]],
-  ['temporaryeffects',['TemporaryEffects',['../class_temporary_effects.html',1,'']]]
-];
