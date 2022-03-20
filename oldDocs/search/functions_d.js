716c8fdcf438e670c417a2eab0d522877ed2ac9c
GitHub Action <github-action@users.noreply.github.com>
2022-03-20 15:38:00 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/functions_d.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['needscharge',['NeedsCharge',['../class_entity_player.html#a56259f35a04f0355c3334357efa80ea8',1,'EntityPlayer']]],
-  ['next',['Next',['../class_r_n_g.html#a57b9f468dee926906b3e2421f90a001b',1,'RNG']]],
-  ['nextvictorylap',['NextVictoryLap',['../class_game.html#a4b5ab3665dd2e6aa9a0125c5e03c4f84',1,'Game']]]
-];
