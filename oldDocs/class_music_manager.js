30c02c5e02ff31394aec5edb992301e86ee50e9e
Zamiell <Zamiell@users.noreply.github.com>
2022-03-20 15:38:40 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_music_manager.js
@@ -0,0 +1,22 @@
+var class_music_manager =
+[
+    [ "Crossfade", "class_music_manager.html#a9dad87ac4e51b7030098bdaf4b3be339", null ],
+    [ "Disable", "class_music_manager.html#af7f459563229692476ac9f534523b9cb", null ],
+    [ "DisableLayer", "class_music_manager.html#ab16a58ca0cb9faef5c976c9239069495", null ],
+    [ "Enable", "class_music_manager.html#a3a3b514ace59c037e1b1aa16e98a693b", null ],
+    [ "EnableLayer", "class_music_manager.html#a739effe2f8b18d48e4198e80cc76898f", null ],
+    [ "Fadein", "class_music_manager.html#addbccc02a3d2b2c0e76100ab544932a4", null ],
+    [ "Fadeout", "class_music_manager.html#aa2e1fc100aa389d146254dfffc7d5776", null ],
+    [ "GetCurrentMusicID", "class_music_manager.html#afc47a6f218b711c774bf82e6db8337ee", null ],
+    [ "GetQueuedMusicID", "class_music_manager.html#acb56b6680acef767f1831f61aac87f5b", null ],
+    [ "IsEnabled", "class_music_manager.html#a77e9344a60fd38f0ce941ac47ba5d19b", null ],
+    [ "IsLayerEnabled", "class_music_manager.html#a30884e0dc14e4d762c396dbc63bc8523", null ],
+    [ "Pause", "class_music_manager.html#a2ea64fc6de345968ac0dbf85f57dba22", null ],
+    [ "PitchSlide", "class_music_manager.html#a1c7a725ec16010599a9077ed5dbff952", null ],
+    [ "Play", "class_music_manager.html#a881ddf7f06a847095be86ecf11045319", null ],
+    [ "Queue", "class_music_manager.html#ad8eb79ba61251c58ab5531691dd4447d", null ],
+    [ "ResetPitch", "class_music_manager.html#aa86fd07226ac35f2bee17303c8023453", null ],
+    [ "Resume", "class_music_manager.html#af991d8c4e994b2ad71003d115839271f", null ],
+    [ "UpdateVolume", "class_music_manager.html#a9ef459c6ee622ef92c04fe063f3996ed", null ],
+    [ "VolumeSlide", "class_music_manager.html#a81558afce5857e1c9cc47ebe988af0d7", null ]
+];
---
+++ oldDocs/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['remarks',['Remarks',['../remarks.html',1,'']]]
+];
