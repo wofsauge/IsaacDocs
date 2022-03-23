19bdac94f7ccc5378b11113afa802c214b9d5ab8
GitHub Action <github-action@users.noreply.github.com>
2022-03-23 21:11:25 +0000

    Removed everything with mike 1.1.2

--- oldDocs/namespace_player_types.js
+++
@@ -1,5 +0,0 @@
-var namespace_player_types =
-[
-    [ "ActiveItemDesc", "class_player_types_1_1_active_item_desc.html", "class_player_types_1_1_active_item_desc" ],
-    [ "PosVel", "class_player_types_1_1_pos_vel.html", "class_player_types_1_1_pos_vel" ]
-];
