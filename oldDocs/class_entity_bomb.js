e11ada711eebff3e8717f3b52b8db59536f00bc5
wofsauge <wofsauge@users.noreply.github.com>
2022-03-25 16:27:32 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_entity_bomb.js
@@ -0,0 +1,8 @@
+var class_entity_bomb =
+[
+    [ "SetExplosionCountdown", "class_entity_bomb.html#a26860e6e16084f94b9fcbcdbad9d8a62", null ],
+    [ "ExplosionDamage", "class_entity_bomb.html#ad0e5b2887ebae55ed701b0be640fe22a", null ],
+    [ "Flags", "class_entity_bomb.html#adc257c6b516638b5716dda1841d133e5", null ],
+    [ "IsFetus", "class_entity_bomb.html#ab9a2db2f960e36cd8a7160462e3153ba", null ],
+    [ "RadiusMultiplier", "class_entity_bomb.html#accc5f6f840a866965464f0cd612f3612", null ]
+];
