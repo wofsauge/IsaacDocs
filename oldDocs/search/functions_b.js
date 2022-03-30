bce1350d9d620168bb40189addd9322ce05109e9
GitHub Action <github-action@users.noreply.github.com>
2022-03-30 20:37:29 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/functions_b.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['lerp',['Lerp',['../class_color.html#ab5aec21730df314963db1194bf6991a2',1,'Color']]],
-  ['load',['Load',['../class_sprite.html#af19f1e97acc045158149b0ac5177cc5d',1,'Sprite']]],
-  ['loadgraphics',['LoadGraphics',['../class_sprite.html#a2d48eda74e29b3068a9933059f49c3a8',1,'Sprite']]],
-  ['loadmoddata',['LoadModData',['../namespace_isaac.html#a526834c43e9f0dbaec961602e39245a7',1,'Isaac']]]
-];
