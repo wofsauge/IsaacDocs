b7cda4fe4100ee08923eb21a462667af2165ad18
GitHub Action <github-action@users.noreply.github.com>
2022-04-17 10:23:32 +0000

    Removed everything with mike 1.1.2

--- oldDocs/group__funcs.js
+++
@@ -1,8 +0,0 @@
-var group__funcs =
-[
-    [ "Game", "group__funcs.html#gae643f2600404d3a34d6236fec678aa7c", null ],
-    [ "MusicManager", "group__funcs.html#gadb9aad240fd3780570efed9201ce28ee", null ],
-    [ "Random", "group__funcs.html#gab0914b8a441bd429bdd637e6b31dfd7c", null ],
-    [ "RandomVector", "group__funcs.html#ga1c1c3aca8b6e9a035bae2c4604c999b2", null ],
-    [ "SFXManager", "group__funcs.html#ga9234017f135540fa61ad6c17a7c9205b", null ]
-];
