57547d7fd890d1c3734eba1969991aacce6bcfdd
GitHub Action <github-action@users.noreply.github.com>
2022-05-04 16:38:50 +0000

    Removed everything with mike 1.1.2

--- oldDocs/namespace_room_config.js
+++
@@ -1,6 +0,0 @@
-var namespace_room_config =
-[
-    [ "Entry", "class_room_config_1_1_entry.html", "class_room_config_1_1_entry" ],
-    [ "Room", "class_room_config_1_1_room.html", null ],
-    [ "Spawn", "class_room_config_1_1_spawn.html", "class_room_config_1_1_spawn" ]
-];
