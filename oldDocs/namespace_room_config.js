5a7272d3d0dd9841bb3d49ac75da517b208869b3
Zamiell <Zamiell@users.noreply.github.com>
2022-05-19 14:17:43 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/namespace_room_config.js
@@ -0,0 +1,6 @@
+var namespace_room_config =
+[
+    [ "Entry", "class_room_config_1_1_entry.html", "class_room_config_1_1_entry" ],
+    [ "Room", "class_room_config_1_1_room.html", null ],
+    [ "Spawn", "class_room_config_1_1_spawn.html", "class_room_config_1_1_spawn" ]
+];
