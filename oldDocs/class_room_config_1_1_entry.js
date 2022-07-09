a18ba74a42b9921b36f2c1e5fabf659cfe0ccb7e
Zamiell <Zamiell@users.noreply.github.com>
2022-07-09 23:43:01 +0000

    Update file(s) "/." from "wofsauge/IsaacDocs"

---
+++ oldDocs/class_room_config_1_1_entry.js
@@ -0,0 +1,7 @@
+var class_room_config_1_1_entry =
+[
+    [ "Subtype", "class_room_config_1_1_entry.html#a4345147a45aebc695660240ddd37c9c6", null ],
+    [ "Type", "class_room_config_1_1_entry.html#a4860f52ad55bf5c584f8d220b89893be", null ],
+    [ "Variant", "class_room_config_1_1_entry.html#a8e879563dc6bf729860c8e0b7f359369", null ],
+    [ "Weight", "class_room_config_1_1_entry.html#a61b848a7ed5acb126e1e79f54ffc8caa", null ]
+];
