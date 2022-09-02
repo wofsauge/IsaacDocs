bc947f07ae051f8a636ab9c6b7aaef1be46e0151
GitHub Action <github-action@users.noreply.github.com>
2022-09-02 16:41:42 +0000

    Removed everything with mike 1.1.2

--- oldDocs/class_shockwave_params.js
+++
@@ -1,9 +0,0 @@
-var class_shockwave_params =
-[
-    [ "Age", "class_shockwave_params.html#a0b6c967382dcc33588d1030a9fa14888", null ],
-    [ "LifeSpan", "class_shockwave_params.html#a735547e9830a4844ca0959488c818dac", null ],
-    [ "Position", "class_shockwave_params.html#a7e4dfcd17d4566a52541c3cdcd8c309f", null ],
-    [ "Strength", "class_shockwave_params.html#aae0759741099f8ff4494a0622cf6bc4d", null ],
-    [ "Time", "class_shockwave_params.html#a2422d035adf40ed3a5b343f077e1aad1", null ],
-    [ "TimeDT", "class_shockwave_params.html#ae439dad53f7605468e1c81aef528373f", null ]
-];
