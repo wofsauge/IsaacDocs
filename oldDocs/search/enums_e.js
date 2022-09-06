dccb2df93770b01110025e5e8a4d88166678991b
GitHub Action <github-action@users.noreply.github.com>
2022-09-06 17:43:40 +0000

    Removed everything with mike 1.1.2

--- oldDocs/search/enums_e.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['roomshape',['RoomShape',['../group__enums.html#gade2a79908923e18471135bfe86d3b1f7',1,'Enums.h']]],
-  ['roomtype',['RoomType',['../group__enums.html#gab540f7414f306325d92272bcef1e34e1',1,'Enums.h']]]
-];
