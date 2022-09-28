97c5cd1b3bef8a8f4dea3cac7bd7f4392760978b
Wofsauge <jan-.-@t-online.de>
2022-09-28 22:25:35 +0200

    reverted search result fix, since its not officially fixed

--- docs/js/customJS.js
+++ docs/js/customJS.js
@@ -341,7 +341,6 @@ document$.subscribe(function() {
             hidePlaceholderChar($(this));
         });
         $("article.md-search-result__article").each(function(e) {
-            $(this).removeClass("md-typeset"); // Remove class that destroys search result layouts
             if ($(this).attr("data-md-score") < 0) {
                 $(this).parent().parent().hide();
             }
